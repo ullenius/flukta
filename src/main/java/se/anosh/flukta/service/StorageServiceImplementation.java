90ea9d6b9690842f51835a102c3a3c2dac5740cf
Anosh D. Ullenius <anosh@anosh.se>
2020-03-29 16:07:50 +0200

    Put uploaded files in a sub-directory

--- src/main/java/se/anosh/flukta/service/StorageServiceImplementation.java
+++ src/main/java/se/anosh/flukta/service/StorageServiceImplementation.java
@@ -3,12 +3,28 @@ package se.anosh.flukta.service;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class StorageServiceImplementation implements StorageService {
+	
+	private static final String UPLOAD_DIR = "upload";
+	
+	
+	public StorageServiceImplementation() throws IOException {
+		
+		Path dir = Paths.get(UPLOAD_DIR);
+		
+		if (!Files.exists(dir)) {
+			Files.createDirectory(dir);
+		}
+		
+	}
 
 	@Override
 	public void store(MultipartFile file) throws IOException {
@@ -16,9 +32,10 @@ public class StorageServiceImplementation implements StorageService {
 		String filename = file.getOriginalFilename();
 		System.out.println("original filename: " + filename);
 		
-		DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(filename));
-		dataOutputStream.write(file.getBytes());
-		dataOutputStream.close();
+		try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(UPLOAD_DIR + "/" + filename))) {
+			dataOutputStream.write(file.getBytes());
+		}
+	
 	}
 	
 	
