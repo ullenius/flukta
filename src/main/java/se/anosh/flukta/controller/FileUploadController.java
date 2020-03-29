e4f759cf6215788dfff6fa59096ed1d3e417a7bd
Anosh D. Ullenius <anosh@anosh.se>
2020-03-29 18:40:54 +0200

    Log exceptions in controller

--- src/main/java/se/anosh/flukta/controller/FileUploadController.java
+++ src/main/java/se/anosh/flukta/controller/FileUploadController.java
@@ -3,6 +3,8 @@ package se.anosh.flukta.controller;
 import java.io.IOException;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +19,12 @@ import se.anosh.flukta.service.StorageService;
 public class FileUploadController {
 
 	private final StorageService service;
+	private final Logger logger;
 
 	@Autowired
 	public FileUploadController(StorageService service) {
 		this.service = service;
+		logger = LoggerFactory.getLogger(FileUploadController.class);
 	}
 
 	@GetMapping("/")
@@ -29,8 +33,8 @@ public class FileUploadController {
 		try {
 			listOfFiles = service.listFiles();
 			return new ModelAndView("upload", "files", listOfFiles);
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException ex) {
+			logger.error("uploadForm (GET)", ex);
 			return new ModelAndView("upload", "message", "Something went terribly wrong! :(");
 		}
 	}
@@ -40,8 +44,8 @@ public class FileUploadController {
 
 		try {
 			service.store(file);
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException ex) {
+			logger.error("upload (POST) - ", ex);
 			return new ModelAndView("upload", "message", "Something went terribly wrong! :(");
 		}
 		return new ModelAndView("redirect:/");
