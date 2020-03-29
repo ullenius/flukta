aeaddd89f368a1a25a14a3b63f1321e262c6eb2c
Anosh D. Ullenius <anosh@anosh.se>
2020-03-29 16:28:07 +0200

    Add CSS. POST-mapping returns ResponseEntity

--- src/main/java/se/anosh/flukta/controller/FileUploadController.java
+++ src/main/java/se/anosh/flukta/controller/FileUploadController.java
@@ -3,10 +3,14 @@ package se.anosh.flukta.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import se.anosh.flukta.service.StorageService;
 
@@ -20,17 +24,21 @@ public class FileUploadController {
 		this.service = service;
 	}
 	
+	@GetMapping("/")
+	public ModelAndView uploadForm() {
+		return new ModelAndView("upload");
+	}
 	
 	@PostMapping("/upload")
-	public String upload(@RequestParam("file") MultipartFile file) {
+	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
 		
 		try {
 			service.store(file);
 		} catch (IOException e) {
 			e.printStackTrace();
-			return "Something went terribly wrong";
+			return new ResponseEntity<String>("Something went terribly wrong", HttpStatus.I_AM_A_TEAPOT);
 		}
-		return "success";
+		return new ResponseEntity<String>("Success!", HttpStatus.ACCEPTED);
 		
 	}
 
